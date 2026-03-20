d57e82306c0b6be5c1512a7f56e75ab675129b53
19220avinash <19220avinash@gmail.com>
2026-03-20 17:03:17 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata = {
-  title: "Ace Portfolio 🚀",
+  title: "Avinash Portfolio 🚀",
   description: "Full Stack Developer Portfolio",
 };
 
